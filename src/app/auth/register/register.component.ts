fbc1a157f72dcd9406404c6cfa867e514b7e7d39
Geronimo <geronimoalejandroanselmo@gmail.com>
2020-07-10 20:24:31 -0300

    Working Front End

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl}  from '@angular/forms'
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm = new FormGroup({
+    email: new FormControl(''),
+    password:new FormControl('')
+  })
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRegister() :void
+  {
+    console.log(this.registerForm.value)
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyC_-kQIg4S1I56CxXw10TdcllM80RXwmu8",
+    authDomain: "login-ng-3dc2f.firebaseapp.com",
+    databaseURL: "https://login-ng-3dc2f.firebaseio.com",
+    projectId: "login-ng-3dc2f",
+    storageBucket: "login-ng-3dc2f.appspot.com",
+    messagingSenderId: "334747415074",
+    appId: "1:334747415074:web:100596227edb9f0a94d07b"
+
+  }
 };
 
 /*
